e5145b9d4c5e88c1578f6ae52b15647871ea7800
mynameiskayky <kaykyvieiraa@icloud.com>
2021-09-23 22:34:17 -0300

    second commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+
+var typed = new Typed('#typed', {
+    stringsElement: '#typed-strings',
+    typeSpeed: 30,
+    showCursor : false ,
+});
+  
+  const sr = ScrollReveal({
+    origin: 'top',
+    distance: '50px',
+    duration: 2000,
+    reset: true
+});
+  
+  ScrollReveal().reveal('.tenis', {
+    delay: 200
+});
